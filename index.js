220ffab3f5682c04857cb1f76c1995bc89282263
ratul081 <ratullinux77@gmail.com>
2023-10-10 23:48:44 +0600

    Services end point finished

--- index.js
+++ index.js
@@ -8,16 +8,18 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 // require('dotenv').config();
 app.use(cors())
 app.use(express.json())
-const data = require('./scr/BhojSala.json')
 
 
+// Database authentication
 const db_User = "volunteer_network"
 const db_Password = "6TP9emlkz7QSbjP4"
 
 
 //MongoDB conection
 const uri = `mongodb+srv://${db_User}:${db_Password}@cluster0.xige0uf.mongodb.net/?retryWrites=true&w=majority`;
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -36,30 +38,106 @@ async function dbConnect() {
 dbConnect()
 
 //Database
-const volunteerServices = client.db('volunteer_network').collection('volunteer_services');
-const volunteerOrders = client.db('volunteer_network').collection('volunteer_orders')
+const database = client.db('b611-service-review-assignment_db')
+const services = database.collection('b611-service-review-assignment_services');
+const servicesReviews = database.collection('b611-service-review-assignment_services_reviews');
+const serviceOrders = database.collection('b611-service-review-assignment_service_orders')
 
-
-
-
-
-
-
-
-
-
-
+//endpoints
 app.get('/', (req, res) => {
   res.send("Bhoj Shala server is running")
 })
-app.get('/services', (req, res) => {
-  const homeData = data.slice(0, 3)
-  res.send(homeData)
+
+
+app.get('/services_home', async (req, res) => {
+  try {
+    const query = {}
+    const cursor = services.find(query)
+    const data = await cursor.limit(3).toArray()
+    // console.log("🚀 ~ file: index.js:64 ~ app.get ~ data:", result)
+    res.send({
+      status: true,
+      massage: "Successfully got the data",
+      data: data
+    })
+  }
+  catch (error) {
+    console.log(error.name.bgRed, error.message.bold);
+    res.send({
+      success: false,
+      error: error.message,
+    });
+  }
 });
-app.get('/foods', (req, res) => {
-  const homeData = data
-  res.send(homeData)
+app.get('/services', async (req, res) => {
+  try {
+    // const page = parseInt(req.query.page)
+    // const size = parseInt(req.query.size)
+    // const skipNumber = page * size
+    const query = {}
+    const cursor = services.find(query)
+    const result = await cursor.toArray()
+    console.log("🚀 ~ file: index.js:64 ~ app.get ~ data:", result)
+    // const cursor = services.find(query)
+    // const count = await services.estimatedDocumentCount()
+    // const data = await cursor.skip(skipNumber).limit(size).toArray()
+    res.send({
+      status: true,
+      massage: "Successfully got the data",
+      // data: { count, services }
+      data: result
+    })
+  }
+  catch (error) {
+    console.log(error.name.bgRed, error.message.bold);
+    res.send({
+      success: false,
+      error: error.message,
+    });
+  }
 });
+
+app.post("/orders", async (req, res) => {
+  try {
+    const user = req.body;
+    console.log("🚀 ~ file: index.js:103 ~ app.post ~ user:", user)
+    res.send(user)
+  }
+  catch (error) {
+    console.log(error.name.bgRed, error.message.bold);
+    res.send({
+      success: false,
+      error: error.message,
+    });
+  }
+})
+app.post("/my_reviews", async (req, res) => {
+  try {
+    const review = req.body;
+    console.log("🚀 ~ file: index.js:103 ~ app.post ~ review:", review)
+    res.send(review)
+  }
+  catch (error) {
+    console.log(error.name.bgRed, error.message.bold);
+    res.send({
+      success: false,
+      error: error.message,
+    });
+  }
+})
+// app.get("/order", async (req, res) => {
+//   try {
+
+//   }
+//   catch (error) {
+//     console.log(error.name.bgRed, error.message.bold);
+//     res.send({
+//       success: false,
+//       error: error.message,
+//     });
+//   }
+// })
+
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
