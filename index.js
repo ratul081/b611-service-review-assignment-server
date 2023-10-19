2c05e80b49028f1e7288c66b53e3d5f03952a170
ratul081 <ratullinux77@gmail.com>
2023-10-19 10:44:05 +0600

    Done

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const uri = process.env.DB_URL;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 
 const client = new MongoClient(uri, {
+  useNewUrlParser: true, useUnifiedTopology: true,
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
